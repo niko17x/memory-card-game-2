f9100bfd7dc9a231a7f828ca4741a6ec60759fee
Niko Shin <nshin17@gmail.com>
2023-04-16 08:32:34 -0700

    Moved shuffle and duplicate functions to Main.js for better component readibility.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,43 +5,46 @@ function Cards(props) {
     const allCards = [];
     props.newCards.map((card, index) =>
       allCards.push(
-        <img className="card-image" key={index} src={card.url} alt="" />
+        <img
+          className="card-image"
+          key={index}
+          src={card.url}
+          alt=""
+          onClick={props.handleClick}
+          image-visibility={card.imageVisibility}
+        />
       )
     );
-    const duplicateAllCards = [allCards, allCards];
-    const shuffledDuplicateAllCards = duplicateAllCards.map((array) => {
-      const newArray = [...array];
-      return shuffleAllCards(newArray);
-    });
-    return shuffledDuplicateAllCards;
+
+    // const duplicateAllCards = [allCards, allCards];
+    // const shuffledDuplicateAllCards = duplicateAllCards.map((array) => {
+    //   const newArray = [...array];
+    //   return shuffleAllCards(newArray);
+    // });
+
+    return allCards;
   };
 
-  // Fisher-Yates shuffle algorithm:
-  const shuffleAllCards = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue, randomIndex;
+  // // Fisher-Yates shuffle algorithm:
+  // const shuffleAllCards = (array) => {
+  //   let currentIndex = array.length;
+  //   let temporaryValue, randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+  //   // While there remain elements to shuffle...
+  //   while (currentIndex !== 0) {
+  //     // Pick a remaining element...
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex -= 1;
 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+  //     // And swap it with the current element.
+  //     temporaryValue = array[currentIndex];
+  //     array[currentIndex] = array[randomIndex];
+  //     array[randomIndex] = temporaryValue;
+  //   }
+  //   return array;
+  // };
 
-    return array;
-  };
-
-  return (
-    <div className="cards--container">
-      {renderCardData()}
-      {/* <div>{props.newCards}</div> */}
-    </div>
-  );
+  return <div className="cards--container">{renderCardData()}</div>;
 }
 
 export default Cards;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,57 @@ import Title from "./Title";
 import Cards from "./Cards";
 import cardData from "../cardData";
 
-function Main() {
-  const [allCards, setAllCards] = React.useState(cardData);
+// ? Why is allCards not logging the correct data that I need?
 
-  const randomizeCardData = () => {
-    setAllCards(cardData);
+function Main() {
+  const [allCards, setAllCards] = React.useState(duplicateAllCards);
+
+  const toggleCardVisibility = (event) => {
+    setAllCards((prevCards) =>
+      prevCards.map((card) => {
+        return card.id === event.target.id
+          ? { ...card, imageVisibility: !card.imageVisibility }
+          : card;
+      })
+    );
+    console.log(allCards);
   };
+
+  const handleClick = (event) => {
+    toggleCardVisibility(event);
+  };
+
+  function duplicateAllCards() {
+    const duplicateCards = [cardData, cardData];
+    const shuffleCards = duplicateCards.map((array) => {
+      const newArray = [...array];
+      return shuffleAllCards(newArray);
+    });
+    const combineCards = [...shuffleCards[0], ...shuffleCards[1]];
+    return combineCards;
+  }
+
+  // Fisher-Yates shuffle algorithm:
+  function shuffleAllCards(array) {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 
   return (
     <div className="main--container">
       <Title />
-      <Cards newCards={allCards} />
+      <Cards newCards={allCards} handleClick={handleClick} />
     </div>
   );
 }
