ad5e11b6413ca9d33e3bec98159c7a2df400485e
Niko Shin <nshin17@gmail.com>
2023-04-16 20:13:42 -0700

    Added id for each object.

--- src/cardData.js
+++ src/cardData.js
@@ -1,21 +1,33 @@
 const cardData = [
   {
+    id: 1,
     url: "../bender.svg",
+    imageVisibility: false,
   },
   {
+    id: 2,
     url: "../mario.svg",
+    imageVisibility: false,
   },
   {
+    id: 3,
     url: "../rick.svg",
+    imageVisibility: false,
   },
   {
+    id: 4,
     url: "../stitch.svg",
+    imageVisibility: false,
   },
   {
+    id: 5,
     url: "../thor.svg",
+    imageVisibility: false,
   },
   {
+    id: 6,
     url: "../woody.svg",
+    imageVisibility: false,
   },
 ];
 
